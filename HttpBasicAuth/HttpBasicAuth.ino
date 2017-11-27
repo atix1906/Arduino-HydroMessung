5895cc9c28c886aabca9afaa8f90f218eea33ad8
atix1906 <th.he.1906@gmail.com>
2017-11-28 00:02:56 +0100

    Add files via upload

---
+++ HttpBasicAuth/HttpBasicAuth.ino
@@ -0,0 +1,72 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#import "config.h"
+
+#define DEBUGPRINT
+#ifdef DEBUGPRINT
+#define DEBUG_PRINT(x) Serial.println (x)
+#else
+#define DEBUG_PRINT(x)
+#endif
+
+//const char* ssid = "Funknetzwerk";
+//const char* password = "#C3106_T1906#$";
+
+ESP8266WebServer server(80);
+
+const char* host = "emoncms.org";
+const char* nodeId = "21";
+
+//const char* www_username = "admin";
+//const char* www_password = "esp8266";
+long startMills = 0;
+
+void setup() {
+ /* Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if(WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Connect Failed! Rebooting...");
+    delay(1000);
+    ESP.restart();
+  }*/
+  startMills = millis();
+
+  Serial.begin(115200);
+
+  DEBUG_PRINT();
+  
+  WiFiManager wifi;
+  
+  wifi.setTimeout(120);
+
+  if(!wifi.autoConnect("Funknetzwerk")){
+    DEBUG_PRINT("timeout - going to sleep");
+    DEBUG_PRINT(millis() - startMills);
+
+    delay(1000);
+
+    ESP.restart();
+  }
+  
+  ArduinoOTA.begin();
+
+  server.on("/", [](){
+    if(!server.authenticate(www_username, www_password))
+      return server.requestAuthentication();
+    server.send(200, "text/plain", "Login OK");
+  });
+  server.begin();
+
+  Serial.print("Open http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/ in your browser to see it working");
+}
+
+void loop() {
+  ArduinoOTA.handle();
+  server.handleClient();
+}
---
+++ HydroMessung.ino
@@ -0,0 +1,79 @@
+/*
+  Analog Input
+
+  Demonstrates analog input by reading an analog sensor on analog pin 0 and
+  turning on and off a light emitting diode(LED) connected to digital pin 13.
+  The amount of time the LED will be on and off depends on the value obtained
+  by analogRead().
+
+  The circuit:
+  - potentiometer
+    center pin of the potentiometer to the analog input 0
+    one side pin (either one) to ground
+    the other side pin to +5V
+  - LED
+    anode (long leg) attached to digital output 13
+    cathode (short leg) attached to ground
+
+  - Note: because most Arduinos have a built-in LED attached to pin 13 on the
+    board, the LED is optional.
+
+  created by David Cuartielles
+  modified 30 Aug 2011
+  By Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/AnalogInput
+*/
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+int ledGreen = 5;
+int ledYellow = 4;
+int ledRed = 3;
+float percentage = 0;
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+//  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin); 
+  percentage = (sensorValue/700.0)*100.0;
+  Serial.print("Sensor Wert: ");
+  Serial.print(sensorValue);
+  Serial.print("\tProzent: ");
+  Serial.println(percentage);
+
+  if(percentage<20.0){
+    digitalWrite(ledGreen, LOW);
+    digitalWrite(ledYellow, LOW);
+    digitalWrite(ledRed, HIGH);
+  }
+  else if(percentage<40.0){
+    digitalWrite(ledGreen, LOW);
+    digitalWrite(ledYellow, HIGH);
+    digitalWrite(ledRed, LOW);
+  }
+  else{
+    digitalWrite(ledGreen, HIGH);
+    digitalWrite(ledYellow, LOW);
+    digitalWrite(ledRed, LOW);
+  }
+  // turn the ledPin on
+  //digitalWrite(ledPin, HIGH);
+  // stop the program for <sensorValue> milliseconds:
+  //delay(sensorValue);
+  // turn the ledPin off:
+  //digitalWrite(ledPin, LOW);
+  // stop the program for for <sensorValue> milliseconds:
+  delay(100);
+}
